357fbf09eb35210e555222a54305e64238128728
kirill302 <72646770+kirill302@users.noreply.github.com>
2021-01-05 10:37:13 +0300

    dcdd

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 
+
+   // intro burger
    $('.intro__burger').click(function() {
       $('.intro__nav,.intro__burger').toggleClass('active');
       $('body').toggleClass('lock');
-
    });
 
 });
+
+// slider
+new Swiper('.swiper-container', {
+   slidesPerView: 'auto',
+
+   spaceBetween: 0,
+   
+   loop: true,
+
+
+   freeMode: true,
+
+});
+
